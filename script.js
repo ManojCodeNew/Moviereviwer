1152fd72c045a0d48c9576c103f587f4d2d1c85d
manoj <manojpoojary2004@gmail.com>
2023-04-20 17:12:51 +0530

    used function

--- script.js
+++ script.js
@@ -4,16 +4,38 @@ const popularmovielink="https://api.themoviedb.org/3/movie/popular?api_key="+api
 let MovieAccordingintognerid="https://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres="+28;
 let getapikey="https://api.themoviedb.org/3/genre/movie/list?api_key="+apikey+"&language=en-US"
 window .addEventListener("load",async ()=>{
-    let pr=await(await fetch(MovieAccordingintognerid)).json();
+    let pr=await(await fetch(getapikey)).json();
+
+    let genrelist=pr.genres;
+    // let genrelength=genrelist.length;
+    if(genrelist.length)
+        genrelist.forEach(genre=>
+        {
+            console.log(genre.name)
+            fullmovie(genre);
+            
+        });
+        async function fullmovie(genre){
+            if(genre.id==28)
+            {
+                console.log("hai hello")
+                return
+            }
+        }
+      console.log("hai")
+    console.log(pr.genres);
     console.log(pr)
-    let halfimglink="http://image.tmdb.org/t/p/w500"
-    let posterpathimg=pr.results[0].poster_path;
-    let fullimglink=halfimglink+posterpathimg;
-    console.log(fullimglink)
+    // console.log(pr)
+    // let halfimglink="http://image.tmdb.org/t/p/w500"
+    // // for (var i=0;i<15;i++)
+    // let posterpathimg=pr.results[0].poster_path;
+    // let fullimglink=halfimglink+posterpathimg;
+    // // console.log(fullimglink)
     
 })
 let imgtag=document.createElement("img");
 imgtag.src="http://image.tmdb.org/t/p/w500/3GrRgt6CiLIUXUtoktcv1g2iwT5.jpg" ;
+// imgtag.src=fullimglink;
 imgtag.width=150;
 imgtag.height=200;
 let posterContainer=document.querySelector(".poster-container");
